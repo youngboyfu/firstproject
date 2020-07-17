c936aca2a2efc34d39def46359a63288eb13015d
付伟 <87162373@qq.com>
2020-07-17 22:28:09 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 懒加载
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
+const Me = () => import('views/me/Me')
 
 Vue.use(VueRouter)
-
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    // 指定的组件
+    component:Home
+  },
+  {
+    path: '/category',
+    component:Category
+  },
+  {
+    path: '/cart',
+    component:Cart
+  },
+  {
+    path: '/me',
+    component:Me
   }
 ]
-
 const router = new VueRouter({
-  routes
+  routes,
+  // url模式
+  mode:'history'
 })
 
 export default router
